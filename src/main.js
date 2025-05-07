b67e6a7270d048df4f4f8e38522cce7b09ca0f0e
JhoannBustamante <JhoannBustamanteFP@gmail.com>
2025-05-06 19:27:52 -0500

    update main,index

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-async function getTrendingMoviesPreview() {
-  const res = await fetch(
-    "https://api.themoviedb.org/3/trending/tv/day?api_key=" + API_KEY
-  );
-  const data = await res.json();
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+  params: {
+    api_key: API_KEY,
+  },
+});
 
+async function getTrendingMoviesPreview() {
+  const { data } = await api("trending/person/day");
   const movies = data.results;
 
   movies.forEach((movie) => {
@@ -19,13 +25,36 @@ async function getTrendingMoviesPreview() {
     movieImg.setAttribute("alt", movie.original_name);
     movieImg.setAttribute(
       "src",
-      "https://image.tmdb.org/t/p/w300" + movie.poster_path
+      "https://image.tmdb.org/t/p/w300" + movie.profile_path
     );
 
     movieContainer.appendChild(movieImg);
     trendingPreviewMoviesContainer.appendChild(movieContainer);
   });
-  console.log({ data, movies });
+}
+
+async function getCategegoriesPreview() {
+  const { data } = await api("genre/tv/list");
+  const categories = data.genres;
+
+  categories.forEach((category) => {
+    const previewCategoriesContainer = document.querySelector(
+      "#categoriesPreview .categoriesPreview-list"
+    );
+
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("category-container");
+
+    const categoryTitle = document.createElement("h3");
+    categoryTitle.classList.add("category-title");
+    categoryTitle.setAttribute("id", "id" + category.id);
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+  });
 }
 
 getTrendingMoviesPreview();
+getCategegoriesPreview();
